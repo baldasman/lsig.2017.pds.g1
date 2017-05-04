d77735a4761f4d919cadb69e4bc97c7aa8e06303
Lucas Reis <reisbelo@uol.com.br>
2017-05-04 12:08:52 +0100

    Tabelas: Profile, Address, Document e Order criadas

--- JoiaDaEuropa/config/routes.rb
+++ JoiaDaEuropa/config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'attachment/index'
+
+  get 'attachment/new'
+
+  get 'attachment/create'
+
+  get 'attachment/destroy'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
--- JoiaDaEuropa/db/schema.rb
+++ JoiaDaEuropa/db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504094919) do
+ActiveRecord::Schema.define(version: 20170504110217) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "country"
     t.string   "zip_code"
     t.string   "street"
     t.string   "number_add"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "attachments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "file"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -35,7 +42,6 @@ ActiveRecord::Schema.define(version: 20170504094919) do
     t.string   "status"
     t.text     "client_comment", limit: 65535
     t.text     "adm_comment",    limit: 65535
-    t.string   "attachment"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.integer  "profile_id"
