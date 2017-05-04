1aa928af53d6490d1eaf7a0265602f17eb526403
Danilo Soares Cardoso <dsc.soares@gmail.com>
2017-05-04 04:07:56 +0100

    Adição de comentários referentes à configuração do OS X.

--- JoiaDaEuropa/db/schema.rb
+++ JoiaDaEuropa/db/schema.rb
@@ -21,20 +21,6 @@ ActiveRecord::Schema.define(version: 20170503230456) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "client_area_order_states", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "client_area_orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.datetime "date"
-    t.decimal  "total",      precision: 10
-    t.integer  "state_id"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-  end
-
   create_table "documents", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "type"
     t.string   "number_doc"
