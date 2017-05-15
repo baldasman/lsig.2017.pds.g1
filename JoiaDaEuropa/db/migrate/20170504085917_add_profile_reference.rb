0b0f26c40217b120eeeaf7ce1a557e12c2fd7904
Pedro Santos <baldasman@gmail.com>
2017-05-15 08:28:48 +0100

    cleanup

--- JoiaDaEuropa/db/migrate/20170504085917_add_profile_reference.rb
+++
@@ -1,7 +0,0 @@
-class AddProfileReference < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :orders, :profile, index: true
-    add_reference :attachments, :profile, index: true
-    add_reference :users, :profile, index: true
-  end
-end
