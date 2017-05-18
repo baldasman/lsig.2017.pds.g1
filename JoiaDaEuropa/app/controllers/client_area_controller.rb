c4d66ca8f1e268359457c6675058e3f449261a0b
guilhermecesar28@gmail.com <guigui08>
2017-05-18 16:31:08 +0100

    Clean up

--- JoiaDaEuropa/app/controllers/client_area_controller.rb
+++ JoiaDaEuropa/app/controllers/client_area_controller.rb
@@ -1,5 +1,8 @@
 class ClientAreaController < ApplicationController
 
+
+    before_action :check_profile
+
     def index
 
         @profile = current_user.profile
@@ -18,13 +21,24 @@ class ClientAreaController < ApplicationController
         @profile.name = params[:profile][:name]
 
         if @profile.save
-            redirect_to client_area_index_path
+            redirect_to client_area_inicio_path
         else
             flash[:error] = @profile.errors.messages
 
             redirect_to client_area_edit_account_path
         end
 
+    end
+
+    def check_profile
+
+        if current_user.profile_id.nil?
+            _profile = Profile.new name: current_user.email, account_type: 1
+            _profile.save
+
+            current_user.profile_id = _profile.id
+            current_user.save
+        end
     end
 
 end
